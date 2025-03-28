562087279d120569d39797f7ad58525e6e533f79
Stanislav <stasikrybcev@gmail.com>
2025-03-28 23:33:35 +0100

    Leraning

---
+++ script.js
@@ -0,0 +1,78 @@
+// "use strict";
+
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+
+// obj.a = 10;
+// console.log(obj.a);
+
+
+
+// const arr = ["a", "b", "c"]  //masiv
+// const arrObj = {
+//     0: "a",
+//     1: "b",
+//     2: "c"
+// };
+
+// const b = "b";
+
+// arrObj.b = "1223"
+// arrObj["b"] = "1223"
+
+// console.log(arrObj["b"]);
+// console.log(arrObj.b);
+
+// const obj = { a: 1, b: 2 } // object
+// const obj = {
+//     'Anna': 500,
+//     'Alise': 800
+// } // object
+
+// console.log(obj);
+
+
+// const storeName = "AppStore";
+
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ["ivan", "dima", "bob"],
+//     products: {
+//         iphone: 1200,
+//         airpods: 100
+//     },
+//     open: true
+// }
+
+
+// alert("SDSD");
+
+// const result = confirm("are u here");
+// console.log(result);
+
+
+// const answer = prompt("u ahve 19", "");
+// console.log(answer);
+
+// const answers = [];
+// answers[0] = prompt("sdfsdf", "");
+// answers[1] = prompt("sdfssfd453df", "");
+// answers[2] = prompt("sdertergerwqsfsdf", "");
+
+// document.write(answers);
+
+const category = "tergrt";
+console.log(`https://sdsds.com/${category}/5`);
+
+const name = "stas";
+alert(`siema, ${name}`);
