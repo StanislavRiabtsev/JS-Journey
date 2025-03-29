ba271edd9d24ceab65be0a8b984a02a5ee515277
Stanislav <stasikrybcev@gmail.com>
2025-03-29 13:48:52 +0100

    Adding obj

--- script.js
+++ script.js
@@ -1,78 +1,21 @@
-// "use strict";
+'use strict';
 
+const numberOfFilms = +prompt("How many movies have you watched already?", "");
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privet: false
+};
 
-// let number = 5;
-// const leftBorderWidth = 1;
+const a = prompt("One of the last movies I watched?", ""),
+    b = prompt("How much would you rate it?", ""),
+    c = prompt("One of the last movies I watched?", ""),
+    d = prompt("How much would you rate it?", "");
 
-// number = 10;
-// console.log(number);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-// const obj = {
-//     a: 50
-// };
-
-
-// obj.a = 10;
-// console.log(obj.a);
-
-
-
-// const arr = ["a", "b", "c"]  //masiv
-// const arrObj = {
-//     0: "a",
-//     1: "b",
-//     2: "c"
-// };
-
-// const b = "b";
-
-// arrObj.b = "1223"
-// arrObj["b"] = "1223"
-
-// console.log(arrObj["b"]);
-// console.log(arrObj.b);
-
-// const obj = { a: 1, b: 2 } // object
-// const obj = {
-//     'Anna': 500,
-//     'Alise': 800
-// } // object
-
-// console.log(obj);
-
-
-// const storeName = "AppStore";
-
-// const storeDescription = {
-//     budget: 10000,
-//     employees: ["ivan", "dima", "bob"],
-//     products: {
-//         iphone: 1200,
-//         airpods: 100
-//     },
-//     open: true
-// }
-
-
-// alert("SDSD");
-
-// const result = confirm("are u here");
-// console.log(result);
-
-
-// const answer = prompt("u ahve 19", "");
-// console.log(answer);
-
-// const answers = [];
-// answers[0] = prompt("sdfsdf", "");
-// answers[1] = prompt("sdfssfd453df", "");
-// answers[2] = prompt("sdertergerwqsfsdf", "");
-
-// document.write(answers);
-
-const category = "tergrt";
-console.log(`https://sdsds.com/${category}/5`);
-
-const name = "stas";
-alert(`siema, ${name}`);
+console.log(personalMovieDB);
