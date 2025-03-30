5dee12e77f8da40625aa435fc64e5f81c1bb3e55
Stanislav <stasikrybcev@gmail.com>
2025-03-30 20:44:32 +0200

    Conditions and Cycles

---
+++ Movie-tracker/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const numberOfFilms = +prompt("How many movies have you watched already?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt("One of the last movies I watched?", ""),
+        b = prompt("How much would you rate it?", "");
+
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("Done");
+    } else {
+        console.log("Error");
+        i--;
+    }
+
+    if (personalMovieDB.count < 10) {
+        console.log("Watched quite a few films");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("You are a classic spectator");
+    } else if (personalMovieDB >= 30) {
+        console.log("You are a movie buff");
+    } else {
+        console.log("Error");
+    }
+
+}
+
+console.log(personalMovieDB);
