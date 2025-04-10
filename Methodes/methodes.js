b2a4a29d33c00c5a6ba9e83f4b387378bb0e3f3d
Stanislav <stasikrybcev@gmail.com>
2025-04-10 22:42:48 +0200

    Code improvements for task 2

--- Methodes/methodes.js
+++ Methodes/methodes.js
@@ -48,31 +48,7 @@ function getCoupeNumber(place) {
     } else if (place === 0 || place > 36) {
         console.log("There are no such seats in the carriage.");
     } else {
-        if (place === 1 || place === 2 || place === 3 || place === 4) {
-            console.log(1);
-        } else if (place === 4 || place === 5 || place === 6 || place === 7) {
-            console.log(2);
-        }
-        else if (place === 8 || place === 9 || place === 10 || place === 11) {
-            console.log(3);
-        }
-        else if (place === 12 || place === 13 || place === 14 || place === 15) {
-            console.log(4);
-        }
-        else if (place === 16 || place === 17 || place === 19 || place === 20) {
-            console.log(5);
-        }
-        else if (place === 21 || place === 22 || place === 23 || place === 24) {
-            console.log(6);
-        }
-        else if (place === 25 || place === 26 || place === 27 || place === 28) {
-            console.log(7);
-        }
-        else if (place === 29 || place === 30 || place === 31 || place === 32) {
-            console.log(8);
-        } else {
-            console.log(9);
-        }
+        console.log(Math.ceil(place / 4));
     }
 }
-getCoupeNumber(23);
+getCoupeNumber(1);
