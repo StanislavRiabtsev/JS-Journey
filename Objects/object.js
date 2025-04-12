af2793569645f2d346a3b13d523936bca29f4b10
Stanislav <stasikrybcev@gmail.com>
2025-04-12 20:07:25 +0200

    Arrays and pseudo-arrays

--- Objects/object.js
+++ Objects/object.js
@@ -7,12 +7,12 @@ const options = {
     colors: {
         border: "black",
         bg: "red"
+    },
+    makeTest: function () {
+        console.log("TEST");
     }
 };
 
-// console.log(options.name);
-// delete options.name;
-// console.log(options);
 
 for (let key in options) {
     if (typeof (options[key]) === 'object') {
@@ -22,5 +22,56 @@ for (let key in options) {
     } else {
         console.log(`Property ${key} has meaning: ${options[key]}`);
     }
-
 }
+
+options.makeTest();
+
+const { border, bg } = options.colors;
+console.log(border);
+
+console.log(Object.keys(options).length);
+
+const about = {
+    name: "Stanislav",
+    lastname: "Riabtsev",
+    learn: "JS",
+    age: 19,
+    language: {
+        ukranian: true,
+        english: "A2",
+        polish: "B2",
+    },
+    hobby: {
+        football: {
+            postion: "forward",
+            time: 6,
+        },
+        computergame: {
+            dota: 1700,
+            cs: 800,
+        }
+    }
+}
+
+const { dota, cs } = about.hobby.computergame;
+console.log(dota);
+console.log(Object.keys(about).length);
+
+console.log(options.name);
+delete options.name;
+console.log(options);
+
+let counter = 0
+for (let key in options) {
+    if (typeof (options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Property ${i} has meaning: ${options[key][i]}`);
+            counter++;
+        }
+    } else {
+        console.log(`Property ${key} has meaning: ${options[key]}`);
+        counter++;
+    }
+}
+
+console.log(counter);
