debf19fe51322c8a79a9fd2ddf744d0ddb9c1fea
Stanislav <stasikrybcev@gmail.com>
2025-04-17 22:37:04 +0200

    OOP Basics

---
+++ OOP Basics/oop.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let str = "some";
+let strObj = new String(str);
+
+console.log(typeof (str));
+console.log(typeof (strObj));
+
+console.dir([1, 2, 4]);
+
+const soldier = {
+    health: 400,
+    armor: 100
+};
+
+const john = {
+    health: 100;
+}
