147a72a99623fcb4ddb07473529a14eb15342cf6
Stanislav <stasikrybcev@gmail.com>
2025-04-21 19:40:19 +0200

    Interview questions to understand the basics

---
+++ Interview tasks/tasks.js
@@ -0,0 +1,29 @@
+"use strict";
+// //1)
+// let x = 5; alert(x++);
+// //2)
+// console.log([] + false - null + true);
+// //3)
+// let y = 1;
+// let x = y = 2;
+// alert(x);
+// //4)
+// console.log([] + 1 + 2);
+// //5)
+// alert("1"[0]);
+// //6)
+// console.log(2 && 1 && null && 0 && undefined);  // && in false but || in true
+// //7)
+// console.log(!!(a && b) === (a && b)); // Its false
+// //8)
+// alert(null || 2 && 3 || 4); // Its 3
+// //9)
+// console.log((a = [1, 2, 3]) === (b = [1, 2, 3]));
+// //10)
+// alert(+"Infinity");
+// //11)
+// const a = "Hedgehog";
+// const b = "apple";
+// console.log(a < b);
+// //12)
+console.log(0 || "" || 2 || undefined || null || false);
--- Lexical Environment/lexical.js
+++ Lexical Environment/lexical.js
@@ -1,1 +1,19 @@
 "use strict";
+
+function createCount() {
+    let counter = 0;
+
+    const myFunction = function () {
+        counter = counter + 1;
+        return counter
+    }
+
+    return myFunction;
+};
+
+const increment = createCount();
+const c1 = increment();
+const c2 = increment();
+const c3 = increment();
+
+console.log(c1, c2, c3);
