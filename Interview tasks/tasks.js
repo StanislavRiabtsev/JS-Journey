717a5bc43e36093a29397dbe8b0aece1016b657f
Stanislav <stasikrybcev@gmail.com>
2025-04-22 14:31:18 +0200

    Coding Exercise 15: Code Error Finding Tasks

--- Interview tasks/tasks.js
+++ Interview tasks/tasks.js
@@ -1,6 +1,6 @@
 "use strict";
 // //1)
-// let x = 5; alert(x++);
+// let r = 5; alert(r++);
 // //2)
 // console.log([] + false - null + true);
 // //3)
@@ -26,4 +26,62 @@
 // const b = "apple";
 // console.log(a < b);
 // //12)
-console.log(0 || "" || 2 || undefined || null || false);
+// console.log(0 || "" || 2 || undefined || null || false);
+
+/////////////////////////////////////////////////
+// Coding Exercise 15: Code Error Finding Tasks
+/////////////////////////////////////////////////
+
+const restorantData = {
+    menu: [
+        {
+            name: 'Salad Caesar',
+            price: '4$'
+        },
+        {
+            name: 'Pizza Diavola',
+            price: '9$'
+        },
+        {
+            name: 'Beefsteak',
+            price: '17$'
+        },
+        {
+            name: 'Napoleon',
+            price: '7$'
+        }
+    ],
+    waitors: [
+        { name: 'Alice', age: 22 }, { name: 'John', age: 24 }
+    ],
+    averageLunchPrice: '20$',
+    openNow: true
+};
+
+function isOpen(prop) {
+    let answer = '';
+    prop ? answer = 'Open' : answer = 'Close';
+
+    return answer;
+}
+
+console.log(isOpen(restorantData.openNow))
+
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+    if (+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)) < +average.slice(0, -1)) {
+        return 'Price is below average';
+    } else {
+        return 'The price is above average';
+    }
+}
+
+console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
+
+function transferWaitors(data) {
+    const copy = Object.assign({}, data);
+
+    copy.waitors = [{ name: 'Mike', age: 32 }];
+    return copy;
+}
+
+console.log(transferWaitors(restorantData));
