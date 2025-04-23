e2247d36372fd1baa823cbffca1206f4dafc7f6c
Stanislav <stasikrybcev@gmail.com>
2025-04-23 15:28:01 +0200

    Getting elements from a page

--- Interview tasks/tasks.js
+++ Interview tasks/tasks.js
@@ -1,32 +1,32 @@
 "use strict";
-// //1)
-// let r = 5; alert(r++);
-// //2)
-// console.log([] + false - null + true);
-// //3)
-// let y = 1;
-// let x = y = 2;
-// alert(x);
-// //4)
-// console.log([] + 1 + 2);
-// //5)
-// alert("1"[0]);
-// //6)
-// console.log(2 && 1 && null && 0 && undefined);  // && in false but || in true
-// //7)
-// console.log(!!(a && b) === (a && b)); // Its false
-// //8)
-// alert(null || 2 && 3 || 4); // Its 3
-// //9)
-// console.log((a = [1, 2, 3]) === (b = [1, 2, 3]));
-// //10)
-// alert(+"Infinity");
-// //11)
-// const a = "Hedgehog";
-// const b = "apple";
-// console.log(a < b);
-// //12)
-// console.log(0 || "" || 2 || undefined || null || false);
+//1)
+let r = 5; alert(r++);
+//2)
+console.log([] + false - null + true);
+//3)
+let y = 1;
+let x = y = 2;
+alert(x);
+//4)
+console.log([] + 1 + 2);
+//5)
+alert("1"[0]);
+//6)
+console.log(2 && 1 && null && 0 && undefined);  // && in false but || in true
+//7)
+console.log(!!(a && b) === (a && b)); // Its false
+//8)
+alert(null || 2 && 3 || 4); // Its 3
+//9)
+console.log((a = [1, 2, 3]) === (b = [1, 2, 3]));
+//10)
+alert(+"Infinity");
+//11)
+const a = "Hedgehog";
+const b = "apple";
+console.log(a < b);
+//12)
+console.log(0 || "" || 2 || undefined || null || false);
 
 /////////////////////////////////////////////////
 // Coding Exercise 15: Code Error Finding Tasks
