8dbb30450a930bb7d8c299f7ec28c17a0b84c1c0
Stanislav <stasikrybcev@gmail.com>
2025-04-30 15:38:02 +0200

    Garbage collector and memory leaks

---
+++ Garbage collector/script.js
@@ -0,0 +1,36 @@
+// function func(){
+//     smth = "String";
+// }
+
+// const someRes = getData();
+// const node = document.querySelector('.class');
+
+// setInterval(function () {
+//     if (node) {
+//         node.innerHTML = someRes;
+
+//     }
+// }, 1000);
+
+
+// function outer() {
+//     const potentiallyHugeArray = [];
+//     return function inner() {
+//         potentiallyHugeArray.push('Hello');
+//         console.log("World");
+//     }
+// }
+// const sayHello = outer();
+
+function createElemnt() {
+    const div = document.createElement('div');
+    div.id = 'test';
+    document.body.append(testDiv);
+}
+
+createElemnt();
+
+function deletElemnt() {
+    document.body.removeChild(document.getElementById('test'));
+}
+deletElemnt();
