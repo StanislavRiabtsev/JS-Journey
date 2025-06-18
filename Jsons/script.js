93694d40454d990a275abb2c3c8079e4b98a8046
Stanislav <stasikrybcev@gmail.com>
2025-06-18 23:33:40 +0200

    jsxon

--- Jsons/script.js
+++ Jsons/script.js
@@ -7,6 +7,16 @@ const persone = {
     }
 };
 
+const persone = {
+    name: 'Alex',
+    tel: '+39394343',
+    parents: {
+        mom: 'Olga',
+        dad: 'Mike'
+    }
+};
+
+
 const clone = JSON.parse(JSON.stringify(persone));
 clone.parents.mom = 'Ann';
 console.log(persone);
