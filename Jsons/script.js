0283a62255cfb5fdbd48880c9833be157de81fa0
Stanislav <stasikrybcev@gmail.com>
2025-06-21 23:13:04 +0200

    Fix

--- Jsons/script.js
+++ Jsons/script.js
@@ -7,16 +7,6 @@ const persone = {
     }
 };
 
-const persone = {
-    name: 'Alex',
-    tel: '+39394343',
-    parents: {
-        mom: 'Olga',
-        dad: 'Mike'
-    }
-};
-
-
 const clone = JSON.parse(JSON.stringify(persone));
 clone.parents.mom = 'Ann';
 console.log(persone);
