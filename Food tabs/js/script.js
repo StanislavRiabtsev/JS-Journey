2d8ef9c56428ea49a1e77e56f12cd2b22f1c8b0e
Stanislav <stasikrybcev@gmail.com>
2025-07-01 00:12:41 +0200

    Implementation of the script for sending data to the server

--- Food tabs/js/script.js
+++ Food tabs/js/script.js
@@ -184,8 +184,8 @@ window.addEventListener('DOMContentLoaded', () => {
                         <div class="menu__item-descr">${this.descr}</div>
                         <div class="menu__item-divider"></div>
                         <div class="menu__item-price">
-                            <div class="menu__item-cost">Цена:</div>
-                            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                            <div class="menu__item-cost">Price:</div>
+                            <div class="menu__item-total"><span>${this.price}</span> uah/day</div>
                         </div>
             `;
             this.parent.append(element);
@@ -195,8 +195,8 @@ window.addEventListener('DOMContentLoaded', () => {
     new MenuCard(
         "img/tabs/vegy.jpg",
         "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.Продукт активных и здоровых людей.Это абсолютно новый продукт с оптимальнойценой и высоким качеством!',
+        'Menu "Fitness"',
+        'Menu "Fitness" - this is a new approach to cooking: more fresh vegetables and fruits. A product for active and healthy people. This is a completely new product with an optimal price and high quality!',
         9,
         '.menu .container',
     ).render();
@@ -204,8 +204,8 @@ window.addEventListener('DOMContentLoaded', () => {
     new MenuCard(
         "img/tabs/elite.jpg",
         "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд.Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        'Menu "Premium"',
+        'In the “Premium” menu we use not only beautiful packaging design, but also high-quality execution of dishes. Red fish, seafood, fruits - a restaurant menu without going to a restaurant!',
         17,
         '.menu .container',
         'menu__item',
@@ -215,12 +215,64 @@ window.addEventListener('DOMContentLoaded', () => {
     new MenuCard(
         "img/tabs/post.jpg",
         "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        'Menu "Lenten"',
+        'The Lenten menu is a careful selection of ingredients: a complete absence of animal products, milk from almonds, oats, coconut or buckwheat, the correct amount of protein due to tofu and imported vegetarian steaks.',
         21,
         '.menu .container',
         'menu__item',
         'big'
     ).render();
 
+    //Forms
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Loading',
+        success: 'Success',
+        failrule: 'Fail'
+    }
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            request.setRequestHeader('Content-type', 'application/json');
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function (value, key) {
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failrule;
+                }
+            })
+        })
+    }
 });
