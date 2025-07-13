fc992102f45a3b33c97171622caa1d83dbe8c101
Stanislav <stasikrybcev@gmail.com>
2025-07-13 16:13:12 +0200

    Making your own mistakes

--- Error/script.js
+++ Error/script.js
@@ -6,17 +6,29 @@ const data = [
         tag: 'div'
     },
     {
-        id: '',
+        id: 'sss',
         tag: 'nav'
     },
     {
         id: 'circle',
-        tag: 'span'
+        tag: ''
     }
 ]
 
-data.forEach(blockObj => {
-    const block = document.createElement(blockObj.tag);
-    block.setAttribute('id', blockObj.id);
-    document.body.append(block);
-})
+try {
+    data.forEach((blockObj, i) => {
+        const block = document.createElement(blockObj.tag);
+
+        if (!blockObj.id) throw new SyntaxError(`Error in number ${i + 1} don't have id`);
+
+        block.setAttribute('id', blockObj.id);
+        document.body.append(block);
+    })
+} catch (e) {
+    if (e.name === 'SyntaxError') {
+        console.error(e.message);
+    } else throw e;
+}
+
+// const err = new SyntaxError('wwwwwww');
+// console.log(err.name, err.message, err.stack);
