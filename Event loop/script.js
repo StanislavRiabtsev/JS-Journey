9f107714112293337939ef58ffa0e1ae05ae7677
Stanislav <stasikrybcev@gmail.com>
2025-07-14 16:38:49 +0200

    Event loop, detailed working of asynchronous and synchronous operations

---
+++ Event loop/script.js
@@ -0,0 +1,27 @@
+// console.log(1);
+
+// setTimeout(() => {
+//     console.log('timeOut');
+// }, 4000);
+
+// setTimeout(() => {
+//     console.log('timeOut-4s');
+// }, 4000);
+
+// console.log(2);
+
+
+// let k = 0
+// function count() {
+//     for (let i = 0; i < 1e9; i++) {
+//         k++;
+//     }
+//     alert('Done');
+// }
+
+// count();
+
+setTimeout(() => {
+    console.log(1);
+}, 0);
+console.log(2);
