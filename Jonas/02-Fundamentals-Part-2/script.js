72d54342b4760f85cbde406c131016330e0597ec
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2025-12-26 14:21:57 +0100

    Functions

--- Jonas/02-Fundamentals-Part-2/script.js
+++ Jonas/02-Fundamentals-Part-2/script.js
@@ -1,10 +1,28 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can drive');
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive');
 
 
-const interface = 'Auido';
+// const interface = 'Auido';
+
+
+function logger() {
+    console.log('My name is Stanislav');
+}
+
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juisce with ${apples} apples and ${oranges} oranges`;
+    return juice
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+const appleOrnageJuice = fruitProcessor(2, 4);
+console.log(appleOrnageJuice);
