39ff59c05713d3b960993607118ff42ad9ea6c0f
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2026-01-22 21:49:54 +0100

    How Passing Arguments Works: Value vs. Reference

--- Jonas/10-Functions/script.js
+++ Jonas/10-Functions/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/* ///////////  Default Parameters ////////////
 const bookings = [];
 
 const createBooking = function (
@@ -22,3 +23,33 @@ const createBooking = function (
 createBooking('LH123');
 createBooking('LH123', 2, 800);
 createBooking('LH123', 5);
+*/
+
+const flight = 'LH234';
+const stanislav = {
+  name: 'Stanislav Riabtsev',
+  passport: 243223452,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 243223452) {
+    alert('Check in ');
+  } else {
+    alert('Wrong passport');
+  }
+};
+// checkIn(flight, stanislav);
+// console.log(flight);
+// console.log(stanislav);
+
+// const flightNum = flight;
+// const passenger = stanislav;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000);
+};
+newPassport(stanislav);
+checkIn(flight, stanislav);
