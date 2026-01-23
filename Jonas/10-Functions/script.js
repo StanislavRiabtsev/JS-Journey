48ca6acf27e73678821efdfd3ebe186f4637ef83
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2026-01-23 15:24:44 +0100

    CHALLENGE #1

--- Jonas/10-Functions/script.js
+++ Jonas/10-Functions/script.js
@@ -104,6 +104,7 @@ const greet1 = greeting1 => {
 };
 */
 
+/* ///////////// Functions /////////////
 const lufthansa = {
   airline: 'Lufthansa',
   iataCode: 'LH',
@@ -186,3 +187,38 @@ const addTaxRate = function (rate) {
 };
 const addVAT2 = addTaxRate(0.23);
 console.log(addVAT2(100));
+*/
+
+// ///////////// CHALLENGE #1 /////////////
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  answer: new Array(4).fill(0),
+  registerNewAnswer() {
+    // Get answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`,
+      ),
+    );
+    // Register
+    typeof answer === 'number' &&
+      answer < this.answer.length &&
+      this.answer[answer]++;
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answer);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answer.join(', ')}`);
+    }
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answer: [5, 2, 3] }, 'string');
