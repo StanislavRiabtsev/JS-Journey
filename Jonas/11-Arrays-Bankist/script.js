34788e9a976d8ded1a6ad1bbe5791208da27ee1f
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2026-01-28 19:43:34 +0100

    The filter Method

--- Jonas/11-Arrays-Bankist/script.js
+++ Jonas/11-Arrays-Bankist/script.js
@@ -159,6 +159,8 @@ movements.forEach(function (mov, i, arr) {
 //   console.log(`${value}: ${value}`);
 // });
 
+////////////// Map //////////////
+/*
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const eurToUsd = 1.1;
 const movementsUSD = movements.map(mov => mov * eurToUsd);
@@ -179,3 +181,19 @@ const movementsDescriptions = movements.map((mov, i, arr) => {
   }
 });
 console.log(movementsDescriptions);
+*/
+
+////////////// Filter //////////////
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+const withdrawals = movements.filter(function (mov) {
+  return mov < 0;
+});
+console.log(withdrawals);
