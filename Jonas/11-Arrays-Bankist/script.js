2b1440c2213325682b34d68ede458f62de52509e
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2026-01-30 21:39:58 +0100

    The Magic of Chaining Methods

--- Jonas/11-Arrays-Bankist/script.js
+++ Jonas/11-Arrays-Bankist/script.js
@@ -224,6 +224,7 @@ console.log(max);
 */
 
 ////////////// CHALLENGE #2 //////////////
+/*
 const calcAverageHumanAge = function (ages) {
   const humanAge = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
   console.log(humanAge);
@@ -236,3 +237,14 @@ const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 
 console.log(avg1, avg2);
+*/
+
+////////////// Chaining Method //////////////
+const eurToUsd = 1.1;
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
