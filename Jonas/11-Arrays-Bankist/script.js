7413df775d767c7165c6910e1132675f7e2c6d36
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2026-01-30 21:53:36 +0100

    CHALLENGE #3

--- Jonas/11-Arrays-Bankist/script.js
+++ Jonas/11-Arrays-Bankist/script.js
@@ -240,6 +240,7 @@ console.log(avg1, avg2);
 */
 
 ////////////// Chaining Method //////////////
+/*
 const eurToUsd = 1.1;
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // PIPELINE
@@ -248,3 +249,16 @@ const totalDepositsUSD = movements
   .map(mov => mov * eurToUsd)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
+*/
+
+////////////// CHALLENGE #3 //////////////
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(avg1, avg2);
