2e8e6d11c12e36403cb283cbe1aea277c08631cf
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2026-02-08 17:30:16 +0100

    Array Methods Practice

--- Jonas/11-Arrays-Bankist/script.js
+++ Jonas/11-Arrays-Bankist/script.js
@@ -510,6 +510,7 @@ labelBalance.addEventListener('click', function () {
 */
 
 ////////////// Non-Destructive Alternatives //////////////
+/*
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const reversedMov = movements.toReversed();
@@ -520,3 +521,66 @@ console.log(reversedMov);
 const newMovements = movements.with(1, 2000);
 
 console.log(newMovements);
+*/
+
+////////////// Array Methods Practice //////////////
+
+// 1.
+const bankDepositSum = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((sum, cur) => sum + cur, 0);
+console.log(bankDepositSum);
+
+// 2.
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+console.log(numDeposits1000);
+
+// Prefixed ++ operator
+let a = 10;
+console.log(a++);
+console.log(a);
+
+// 3.
+const { deposits, withdrawals } = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+      sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+      return sums;
+    },
+    {
+      deposits: 0,
+      withdrawals: 0,
+    },
+  );
+console.log(deposits, withdrawals);
+
+// 4.
+// this is a nice title -> This Is a Nice Title
+const convertTitleCase = function (title) {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+  const expectations = [
+    'a',
+    'an',
+    'and',
+    'the',
+    'but',
+    'or',
+    'on',
+    'in',
+    'with',
+  ];
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (expectations.includes(word) ? word : capitalize(word)))
+    .join(' ');
+  return capitalize(title);
+};
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
