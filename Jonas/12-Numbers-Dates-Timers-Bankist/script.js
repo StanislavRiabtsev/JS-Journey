56db1f43ffb33628882119d5ef2b3c053b5606cd
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2026-02-11 21:51:00 +0100

    Math and Rounding

--- Jonas/12-Numbers-Dates-Timers-Bankist/script.js
+++ Jonas/12-Numbers-Dates-Timers-Bankist/script.js
@@ -252,6 +252,8 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 // LECTURES
 
+//////////////// Converting and Checking Numbers ///////////////
+/*
 // Conversion
 console.log(23 === 23.0);
 console.log(Number('23'));
@@ -282,3 +284,41 @@ console.log(Number.isNaN(23 / 0));
 console.log(Number.isInteger(23));
 console.log(Number.isInteger(23.0));
 console.log(Number.isInteger(23 / 0));
+*/
+
+//////////////// Math and Rounding ///////////////
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(Math.max(5, 18, 2, 11, 3));
+console.log(Math.max(5, '18', 2, 11, 3));
+console.log(Math.max(5, '23p', 2, 11, 3));
+
+console.log(Math.min(5, 18, 2, 11, 3));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(randomInt(10, 20));
+console.log(randomInt(0, 3));
+
+// Rounding integers
+console.log(Math.trunc(23.3));
+
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+
+console.log(Math.floor(23.3));
+console.log(Math.floor(23.9));
+
+// Rounding decimals
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log(+(2.345).toFixed(2));
