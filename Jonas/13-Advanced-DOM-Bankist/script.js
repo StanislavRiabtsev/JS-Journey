076fd2a25adf794bafe1dd214d48c98d36b877f3
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2026-02-18 15:42:51 +0100

    Types of Events and Event Handlers

--- Jonas/13-Advanced-DOM-Bankist/script.js
+++ Jonas/13-Advanced-DOM-Bankist/script.js
@@ -117,6 +117,7 @@ logo.className = 'stanislav';
 */
 
 ///////////// Implementing Smooth Scrolling /////////////
+/*
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
@@ -144,3 +145,17 @@ btnScrollTo.addEventListener('click', function (e) {
   // });
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+*/
+
+///////////// Types of Events and Event Handlers /////////////
+const h1 = document.querySelector('h1');
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading');
+};
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener: Great! You are reading the heading');
+// };
