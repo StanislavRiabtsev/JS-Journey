e9ecf18f843d1664ab56d8976f91ecfd1c52ee9e
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2026-02-24 21:16:17 +0100

    Prototypes

--- Jonas/14-OOP/script.js
+++ Jonas/14-OOP/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//////////////// Constructor Functions and the new Operator //////////////
+
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
@@ -25,3 +27,20 @@ const jay = 'Jay';
 
 console.log(stanislav instanceof Person);
 console.log(jay instanceof Person);
+
+//////////////// Prototypes //////////////
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+stanislav.calcAge();
+matilda.calcAge();
+
+console.log(stanislav.__proto__);
+console.log(stanislav.__proto__ === Person.prototype);
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jack, matilda);
+
+console.log(stanislav.hasOwnProperty('firstName'));
+console.log(stanislav.hasOwnProperty('species'));
