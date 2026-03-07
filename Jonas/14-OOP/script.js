cd09d7403862cf76885f73652396e9bdbc773e13
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2026-03-07 23:01:56 +0100

    CHALLENGE #2

--- Jonas/14-OOP/script.js
+++ Jonas/14-OOP/script.js
@@ -96,7 +96,7 @@ mercedes.brake();
 */
 
 //////////////// ES6 Classes //////////////
-
+/*
 // class expression
 // const PersonCl = class{}
 
@@ -183,3 +183,36 @@ console.log(steven.__proto__);
 
 const sarah = Object.create(PersonProto);
 sarah.init('Sarah', 1979);
+*/
+
+//////////////// CHALLENGE #2 //////////////
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new CarCl('Ford', 120);
+ford.accelerate();
+console.log(ford.speedUS);
+ford.speedUS = 50;
+console.log(ford.speedUS);
