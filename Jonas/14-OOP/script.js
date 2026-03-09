1f0836ec3cf46ae68b000ce5aad00d29b69dd068
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2026-03-09 21:01:41 +0100

    CHALLENGE #3

--- Jonas/14-OOP/script.js
+++ Jonas/14-OOP/script.js
@@ -220,7 +220,7 @@ console.log(ford.speedUS);
 */
 
 //////////////// Inheritance Between "Classes": Constructor Functions //////////////
-
+/*
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
@@ -246,3 +246,45 @@ const mike = new Student('Mike', 2020, 'Computer Science');
 console.log(mike);
 mike.introduce();
 mike.calcAge();
+*/
+
+//////////////// CHALLENGE #3 //////////////
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+// Link the prototypes
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} is going at ${this.speed}km/h, with a charge of ${this.charge}`,
+  );
+};
+
+const tesla = new EV('Tesla', 120, 23);
+tesla.chargeBattery(90);
+console.log(tesla);
+tesla.brake();
+tesla.accelerate();
