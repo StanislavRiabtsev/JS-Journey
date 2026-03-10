e64b63c536f25d8c6a192e58eb76a1b8cc0b20e0
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2026-03-10 21:04:55 +0100

    Another Class Example

--- Jonas/14-OOP/script.js
+++ Jonas/14-OOP/script.js
@@ -348,6 +348,7 @@ martha.caclAge();
 */
 
 //////////////// Inheritance Between "Classes": Object.create //////////////
+/*
 const PersonProto = {
   caclAge() {
     console.log(2037 - this.birthYear);
@@ -374,3 +375,46 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
 jay.caclAge();
+*/
+
+//////////////// Another Class Example //////////////
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+  widthraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account(' Jonas', 'EUR', 1111);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+
+acc1.deposit(250);
+acc1.widthraw(140);
+acc1.requestLoan(1000);
+
+console.log(acc1);
