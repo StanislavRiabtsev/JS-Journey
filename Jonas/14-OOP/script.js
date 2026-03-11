d89df14836bead25a8646f45f6a866b8fa0172a1
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2026-03-11 21:57:05 +0100

    CHALLENGE #4

--- Jonas/14-OOP/script.js
+++ Jonas/14-OOP/script.js
@@ -422,7 +422,7 @@ console.log(acc1);
 */
 
 //////////////// Encapsulation: Private Class Fields and Methods //////////////
-
+/*
 // 1. Public fields
 // 2. Private fields
 // 3. Public methods
@@ -492,3 +492,66 @@ acc1
 
 console.log(acc1);
 Account.test();
+*/
+//////////////// CHALLENGE #4 //////////////
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+    return this;
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+    return this;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class EVCl extends CarCl {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `${this.make} is going at ${this.speed} km/h, with a charge of ${
+        this.#charge
+      }`,
+    );
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+rivian
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .chargeBattery(50)
+  .accelerate();
+
+console.log(rivian.speedUS);
